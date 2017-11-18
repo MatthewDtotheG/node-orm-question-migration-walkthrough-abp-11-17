c30e3657d98796084f1d8cf89d072fa7d295e2c5
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-18 20:05:54 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -4,9 +4,9 @@ const db = require('../config/db');
 
 class Question {
   static CreateTable() {
-    return new Promise(function(resolve){
-      resolve("This Does Nothing!")
-    })
+    return new Promise(function(resolve) {
+      resolve('This Does Nothing!');
+    });
   }
 }
 
